ebe4722e9826f24fd660e3dbd98c933f71179936
Sean T. McBeth <sean.mcbeth@gmail.com>
2014-11-14 11:21:06 -0500

    Added identity column to ScriptStatus table to satisfy SQL Azure

--- SqlSiphon/ScriptStatus.cs
+++ SqlSiphon/ScriptStatus.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SqlSiphon.Mapping;
 
 namespace SqlSiphon
 {
-    [Mapping.MappedClass]
+    [MappedClass]
     public class ScriptStatus
     {
+        [AutoPK]
+        public int ScriptID { get; set; }
         public string Script { get; set; }
-        [Mapping.MappedProperty(DefaultValue = "getdate()")]
+        [MappedProperty(DefaultValue = "getdate()")]
         public DateTime RanOn { get; set; }
     }
 }
